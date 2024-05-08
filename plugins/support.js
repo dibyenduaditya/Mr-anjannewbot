b497ee6d96c88136f4212d8d343bf69043344828
🌟⭐ ᴹ🅡.ɑ𝖓𝐣ａ𝖓 🌟⭐ <163623222+Mranjan101@users.noreply.github.com>
2024-05-08 21:28:19 +0530

    Update support.js

--- plugins/support.js
+++ plugins/support.js
@@ -7,7 +7,7 @@ command({
     type: "support"
 }, async (message) => {
     const name = '〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»🎈', title = "〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢» 𝙎𝙐𝙋𝙋𝙊𝙍𝙏🦋", number = '919883457657', body = "〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»";
-    const image = "https://i.imgur.com/V2DZbd7.jpeg", sourceUrl = 'https://github.com/Rudraraj3/Mr-anjannewbot';
+    const image = "https://i.imgur.com/V2DZbd7.jpeg", sourceUrl = 'https://chat.whatsapp.com/HBf3zvaGyRaEfMZlKgUSPV';
     const logo = await getBuffer(image);
     const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nORG: 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»;\nTEL;type=CELL;type=VOICE;waid=${number}:${number}\nEND:VCARD`;
     const adon = { title, body, thumbnail: logo, mediaType: 1, mediaUrl: sourceUrl, sourceUrl, showAdAttribution: true, renderLargerThumbnail: false };
