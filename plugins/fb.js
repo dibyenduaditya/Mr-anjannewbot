06abc868f3f0f951ae83d3fb07cafe228078e327
Rudraraj3 <147349281+Rudraraj3@users.noreply.github.com>
2024-05-08 17:13:18 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -20,7 +20,7 @@ await message.client.sendMessage(message.jid, { text: `\n╔┅┅┅┅┅┅�
 ║
 ║ 𝗦𝗲𝗻𝗱 𝗮 𝗻𝘂𝗺𝗯𝗲𝗿 𝗮𝘀 𝗮 𝗿𝗲𝗽𝗹𝘆 𝘁𝗼 𝗱𝗼𝘄𝗻𝗹𝗼𝗮𝗱
 ╚┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅┅⚆\n`, contextInfo: { externalAdReply: {
-     title: "𝐍𝐄𝐙𝐔𝐊𝐎 𝐌𝐃 ࿊",
+     title: "〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»",
      body: "",
      sourceUrl: "",
      mediaUrl: "",
@@ -45,7 +45,7 @@ let final = message.reply_message.text.split("║ ")[3]
 final = final.replace("𝐔𝐫𝐥 :", "")
 let title = final;
 let {data} = await getJson(`https://api.vihangayt.asia/downloader/fb?url=${title}`)
-await message.sendFromUrl(data.sdLink, {caption: "𝐍𝐄𝐙𝐔𝐊𝐎 𝐌𝐃 ࿊"}, {quoted:message})
+await message.sendFromUrl(data.sdLink, {caption: "〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»"}, {quoted:message})
 
 }catch(error){
 return error
@@ -57,7 +57,7 @@ let final = message.reply_message.text.split("║ ")[3]
 final = final.replace("𝐔𝐫𝐥 :", "")
 let title = final;
 let {data} = await getJson(`https://api.vihangayt.asia/downloader/fb?url=${title}`)
-await message.sendFromUrl(data.hdLink, {caption: "𝐍𝐄𝐙𝐔𝐊𝐎 𝐌𝐃 ࿊"}, {quoted:message})
+await message.sendFromUrl(data.hdLink, {caption: "〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»"}, {quoted:message})
 
 }catch(error){
 return error
