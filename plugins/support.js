d5442ec9482a1df528383d153249da97ace755af
Rudraraj3 <147349281+Rudraraj3@users.noreply.github.com>
2024-05-08 17:28:21 +0530

    Update support.js

--- plugins/support.js
+++ plugins/support.js
@@ -3,13 +3,13 @@ const { getJson, getBuffer, command, isPrivate, sleep } = require("../lib/");
 command({
     pattern: "help",
     fromMe: isPrivate,
-    desc: "nezuko Support",
+    desc: "Anjan support",
     type: "support"
 }, async (message) => {
-    const name = '𝐆𝐎𝐃 𝐙𝐄𝐍𝐈𝐓𝐒𝐔🎈', title = "𝙉𝙀𝙕𝙐𝙆𝙊 𝙎𝙐𝙋𝙋𝙊𝙍𝙏🦋", number = '917907387121', body = "𝐆𝐎𝐃 𝐙𝐄𝐍𝐈𝐓𝐒𝐔🌚";
-    const image = "https://graph.org/file/3879cf1910f65bd8457d7.jpg", sourceUrl = 'https://github.com/godzenitsu/NEZUKO-V2';
+    const name = '〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»🎈', title = "〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢» 𝙎𝙐𝙋𝙋𝙊𝙍𝙏🦋", number = '919883457657', body = "〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»";
+    const image = "https://i.imgur.com/V2DZbd7.jpeg", sourceUrl = 'https://github.com/Rudraraj3/Mr-anjannewbot';
     const logo = await getBuffer(image);
-    const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nORG: 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 𝐍𝐞𝐳𝐮𝐤𝐨;\nTEL;type=CELL;type=VOICE;waid=${number}:${number}\nEND:VCARD`;
+    const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nORG: 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»;\nTEL;type=CELL;type=VOICE;waid=${number}:${number}\nEND:VCARD`;
     const adon = { title, body, thumbnail: logo, mediaType: 1, mediaUrl: sourceUrl, sourceUrl, showAdAttribution: true, renderLargerThumbnail: false };
     await message.client.sendMessage(message.jid, { contacts: { displayName: name, contacts: [{ vcard }] }, contextInfo: { externalAdReply: adon } }, { quoted: message });
 });
