b101f5302ac155bf2539a23f638c9126a0141df3
🌟⭐ ᴹ🅡.ɑ𝖓𝐣ａ𝖓 🌟⭐ <163623222+Mranjan101@users.noreply.github.com>
2024-05-10 09:23:50 +0530

    Delete plugins/Mee.js

--- plugins/Mee.js
+++
@@ -1,1 +0,0 @@
-const { System } = require("../lib/");   System({ pattern: "mee", fromMe: true, desc: "self mention", type: "user", }, async (message, match) => { await message.client.sendMessage(message.chat, { text: `@${message.sender.split("@")[0]}`, mentions: [message.sender] }) });
