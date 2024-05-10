89aab4298264375be76bc43699c92d563e22c77a
🌟⭐ ᴹ🅡.ɑ𝖓𝐣ａ𝖓 🌟⭐ <163623222+Mranjan101@users.noreply.github.com>
2024-05-10 11:24:23 +0530

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 const X = require("../config");
 
 command(
-    {
+        {
         pattern: "Play",
         fromMe: isPrivate,
         desc: "Song Downloader",
@@ -15,7 +15,7 @@ command(
         if (!match) return await message.sendMessage("*_Need Song Name Or Url_*");
 var res = await axios.get(`https://api-viper-x.koyeb.app/api/song?name=${match}`)
 var song = res.data
-await message.client.sendMessage(message.jid, { quoted: message})
+await message.client.sendMessage(message.jid, { text: `*_Downloading ${song.data.title}_*` },{ quoted: message})
 const aswinsparky = await (await fetch(`${song.data.downloadUrl}`)).buffer()
 await message.client.sendMessage(message.jid, { audio :aswinsparky,  mimetype:"audio/mpeg", contextInfo: { externalAdReply: {
 title: "〆͎𝐌𝐫-𝐚𝐧𝐣𝐚𝐧👀🦋 ː͢»",
